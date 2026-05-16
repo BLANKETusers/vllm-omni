9d6597ff292444be85565fb8c42a20208551f536
zzh <943967662@qq.com>
2026-05-16 16:27:09 +0800

    edit file path:tests/e2e/accuracy/test_hunyuan_image3_pixel_accuracy.py
    Signed-off-by: zzh <943967662@qq.com>

--- tests/e2e/accuracy/test_hunyuan_image3_pixel_accuracy.py
+++ tests/e2e/accuracy/test_hunyuan_image3_pixel_accuracy.py
@@ -25,7 +25,7 @@ PROMPT = "A brown and white dog is running on the grass."
 MEAN_THRESHOLD = 0.02
 P99_THRESHOLD = 0.10
 
-_REPO_ROOT = Path(__file__).resolve().parent.parent.parent
+_REPO_ROOT = Path(__file__).resolve().parent.parent.parent.parent
 BASELINE_PATH = _REPO_ROOT / "assets" / "hunyuan" / "hunyuan_baseline.png"
 _DEFAULT_DEPLOY_CONFIG = _REPO_ROOT / "vllm_omni" / "deploy" / "hunyuan_image3.yaml"
 _OFFLINE_SCRIPT = _REPO_ROOT / "examples" / "offline_inference" / "hunyuan_image3" / "end2end.py"
